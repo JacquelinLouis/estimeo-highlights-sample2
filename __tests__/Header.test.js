4ebf74eaa393b92050718633ea7c0778aed90289
jacque_l <jacquelinlouis@gmail.com>
2017-07-03 16:50:25 +0200

    Add first test

--- __tests__/Header.test.js
+++ __tests__/Header.test.js
@@ -5,13 +5,21 @@ import { shallow } from 'enzyme'
 import React from 'react'
 
 import Header from '../components/Header';
-
+/*
+describe('Index.js page', () => {
+    it('has a "<p>" tag', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('p').length).toBe(1)
+    })
+});
+*/
 describe('<Header />', () => {
 
     /**
      * TODO
      */
-    it('will fail', () => {
-        expect(false).toBe(true)
+    it('has a "<title>" tag', () => {
+        const wrapper = shallow(<Header />);
+        expect(wrapper.find('title').text()).toBe('Estimeo Highlights')
     })
-})
+});
