12978b9827e5d2b232e7ecdba144ad22f3825fb6
jacque_l <jacquelinlouis@gmail.com>
2017-07-03 17:09:59 +0200

    Add test correction

--- __tests__/Header.test.js
+++ __tests__/Header.test.js
@@ -14,12 +14,23 @@ describe('Index.js page', () => {
 });
 */
 describe('<Header />', () => {
-
-    /**
-     * TODO
-     */
-    it('has a "<title>" tag', () => {
+    it('has "Estimeo Highlights" as title', () => {
         const wrapper = shallow(<Header />);
         expect(wrapper.find('title').text()).toBe('Estimeo Highlights')
+    });
+
+    it('has font roboto registered', () => {
+        const wrapper = shallow(<Header />);
+        expect(wrapper.contains(<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />)).toBe(true)
+    });
+
+    it('has custom css registered', () => {
+        const wrapper = shallow(<Header />);
+        expect(wrapper.contains(<link rel="stylesheet" href="/static/css/estimeo-style.css"/>)).toBe(true)
+    });
+
+    it('has style tag', () => {
+        const wrapper = shallow(<Header />);
+        expect(wrapper.find('style').length).toBe(1)
     })
 });
--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ export default () => (
         <title>Estimeo Highlights</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
-        <link rel="stylesheet" href="/static/css/estimeostyle.css"/>
+        <link rel="stylesheet" href="/static/css/estimeo-style.css"/>
         <style> {'body {font-family: Roboto;background-color: #F5F5F5;}'}</style>
     </Head>
 )
